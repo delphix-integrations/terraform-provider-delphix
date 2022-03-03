f1afdad961db41edf8a5852bf89736804cdfe26e
Uddipaan <89963410+Uddipaan-Hazarika@users.noreply.github.com>
2022-03-03 13:11:35 +0530

    formatting changes, error handling for polling

--- examples/vdb/main.tf
+++ examples/vdb/main.tf
@@ -1,21 +1,20 @@
 terraform {
-required_providers {
+  required_providers {
     delphix = {
       version = "0.0-dev"
-      source  = "delphix.com/local/delphix"
+      source  = "delphix.com/local/dct"
     }
   }
 }
 
 provider "delphix" {
-  key = "API_KEY"
+  key  = "API_KEY"
   host = "HOST_NAME"
 }
 
-resource "delphix_resource_vdb" "test_vdb" {
-  provider = delphix
-    vdb {
-        auto_select_repository = true
-        source_data_id = "DSOURCE_ID"
-    }
+resource "delphix_vdb" "test_vdb" {
+  vdb {
+    auto_select_repository = true
+    source_data_id         = "DSOURCE_ID"
+  }
 }
