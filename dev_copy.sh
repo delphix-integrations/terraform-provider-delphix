7f5c27e4e81102891a681242cf43fa8cca918b1d
Sunny Suthar <sunny.suthar@delphix.com>
2022-04-05 14:51:55 -0700

    Change versions

--- dev_copy.sh
+++ dev_copy.sh
@@ -2,6 +2,6 @@
 
 # Script to copy build binaries to terraform plugins location
 
-rm -rf ~/.terraform.d/plugins/delphix.com/local/delphix/0.0-dev/$CP_TARGET/
-mkdir -p ~/.terraform.d/plugins/delphix.com/local/delphix/0.0-dev/$CP_TARGET/
-cp $CP_PATH ~/.terraform.d/plugins/delphix.com/local/delphix/0.0-dev/$CP_TARGET/
+rm -rf ~/.terraform.d/plugins/delphix.com/dct/delphix/$CP_VERSION/$CP_TARGET/
+mkdir -p ~/.terraform.d/plugins/delphix.com/dct/delphix/$CP_VERSION/$CP_TARGET/
+cp $CP_PATH ~/.terraform.d/plugins/delphix.com/dct/delphix/$CP_VERSION/$CP_TARGET/
