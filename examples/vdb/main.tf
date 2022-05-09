79539b3690616ea36ebe442983699c3cb4428594
Eyal Kaspi <eyal.kaspi@delphix.com>
2022-05-09 20:14:48 +0200

    Rever tmain changes

--- examples/vdb/main.tf
+++ examples/vdb/main.tf
@@ -13,16 +13,37 @@ provider "delphix" {
   host              = "HOSTNAME"
 }
 
-resource "delphix_vdb" "vdb_name2" {
+resource "delphix_vdb" "vdb_name" {
   provision_type         = "snapshot"
   auto_select_repository = true
   source_data_id         = "dsource"
-  vdb_name = "vdb-from-tf2"
-  oracle_instance_name = "vdbtf"
-  database_name = "vdbtf"
-  unique_name = "vdbtf"
-  db_username = "oracle"
-  db_password = "oracle"
-  template_id = "myTemplate"
-  listener_ids = ["LISTENER"]
+
+  pre_refresh {
+    name    = "n"
+    command = "time"
+    shell   = "bash"
+  }
+  pre_refresh {
+    name    = "n2"
+    command = "time"
+    shell   = "bash"
+  }
+}
+
+resource "delphix_vdb" "vdb_name2" {
+  provision_type         = "timestamp"
+  auto_select_repository = true
+  source_data_id         = "dsource2"
+  timestamp              = "2021-05-01T08:51:34.148000+00:00"
+
+  post_refresh {
+    name    = "n"
+    command = "time"
+    shell   = "bash"
+  }
+  post_refresh {
+    name    = "n2"
+    command = "time"
+    shell   = "bash"
+  }
 }
