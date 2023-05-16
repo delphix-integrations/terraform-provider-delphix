02a452c5330a8162d50d967b7753a052bc7125bd
Uddipaan <89963410+Uddipaan-Hazarika@users.noreply.github.com>
2023-05-16 17:36:26 +0530

    HUBS-1695 | Add example documentation for database specific VDB provision parameters.

---
+++ examples/vdb/mssql/timestamp/main.tf
@@ -0,0 +1,139 @@
+terraform {
+  required_providers {
+    delphix = {
+      version = "VERSION"
+      source  = "delphix-integrations/delphix"
+    }
+  }
+}
+
+provider "delphix" {
+  tls_insecure_skip = true
+  key               = "1.XXXX"
+  host              = "HOSTNAME"
+}
+
+resource "delphix_vdb" "example" {
+  timestamp                       = ""
+  timestamp_in_database_timezone  = ""
+  name                    = "vdb_to_be_created"
+  source_data_id          = "dsource-name"
+  vdb_restart             = true
+  environment_id          = "env-name"
+  environment_user_id     = "environment_user_name"
+  target_group_id         = "group-123"
+  snapshot_policy_id      = "test_snapshot_policy"
+  database_name           = "dbname_to_be_created"
+  mount_point             = "/var/mnt"
+  auto_select_repository  = true
+  engine_id               = "1"
+  retention_policy_id     = "test_retention_policy"
+  custom_env_files = ["/export/home/env_file_1"]
+  custom_env_vars = {
+    MY_ENV_VAR1 = "$HOME"
+    MY_ENV_VAR2 = "$CRS_HOME/after"
+  }
+  recovery_model = ""
+  cdc_on_provision = true
+  repository_id = ""
+
+  config_params jsonencode({
+    processes = 150
+  })
+  make_current_account_owner = true
+  tags {
+    key   = "key-1"
+    value = "value-1"
+  }
+  post_snapshot {
+    name            = "string"
+    command         = "string"
+    shell           = "bash"
+    element_id      = "string"
+    has_credentials = true
+  }
+  post_script {
+    name            = "string"
+    command         = "string"
+    shell           = "bash"
+    element_id      = "string"
+    has_credentials = true
+  }
+  pre_snapshot {
+    name            = "string"
+    command         = "string"
+    shell           = "bash"
+    element_id      = "string"
+    has_credentials = true
+  }
+  pre_stop {
+    name            = "string"
+    command         = "string"
+    shell           = "bash"
+    element_id      = "string"
+    has_credentials = true
+  }
+  post_start {
+    name            = "string"
+    command         = "string"
+    shell           = "bash"
+    element_id      = "string"
+    has_credentials = true
+  }
+  pre_rollback {
+    name            = "string"
+    command         = "string"
+    shell           = "bash"
+    element_id      = "string"
+    has_credentials = true
+  }
+  pre_start {
+    name            = "string"
+    command         = "string"
+    shell           = "bash"
+    element_id      = "string"
+    has_credentials = true
+  }
+  pre_refresh {
+    name            = "string"
+    command         = "string"
+    shell           = "bash"
+    element_id      = "string"
+    has_credentials = true
+  }
+  pre_script {
+    name            = "string"
+    command         = "string"
+    shell           = "bash"
+    element_id      = "string"
+    has_credentials = true
+  }
+  post_refresh {
+    name            = "string"
+    command         = "string"
+    shell           = "bash"
+    element_id      = "string"
+    has_credentials = true
+  }
+  post_stop {
+    name            = "string"
+    command         = "string"
+    shell           = "bash"
+    element_id      = "string"
+    has_credentials = true
+  }
+  configure_clone {
+    name            = "string"
+    command         = "string"
+    shell           = "bash"
+    element_id      = "string"
+    has_credentials = true
+  }
+  post_rollback {
+    name            = "string"
+    command         = "string"
+    shell           = "bash"
+    element_id      = "string"
+    has_credentials = true
+  }
+}
