a54be9ab48bc5900cc09d128da1e695c13d123b6
ankit.patil <ankit.patil@delphix.com>
2024-03-01 16:10:59 +0530

    example for sorce resource

---
+++ examples/source/main.tf
@@ -0,0 +1,26 @@
+/**
+* Summary: This template showcases the properties available when creating an source.
+*/
+
+terraform {
+  required_providers {
+    delphix = {
+      version = "VERSION"
+      source  = "delphix-integrations/delphix"
+    }
+  }
+}
+
+provider "delphix" {
+  tls_insecure_skip = true
+  key               = "1.XXXX"
+  host              = "HOSTNAME"
+}
+
+resource "delphix_source" "source" {
+  name             = "test"
+  repository_value = "REPO-1"
+  engine_value = "2"
+  environment_value = "ENV-1"
+}
+
