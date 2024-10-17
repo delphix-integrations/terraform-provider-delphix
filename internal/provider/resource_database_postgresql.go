5265ab7497f299b8d38ec60d7baf7ce14f7abe40
Uddipaan Hazarika <uddipaan.hazarika@perforce.com>
2024-10-17 15:44:59 +0530

    version bump fpr dct sdk go v22

--- internal/provider/resource_database_postgresql.go
+++ internal/provider/resource_database_postgresql.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"net/http"
 
-	dctapi "github.com/delphix/dct-sdk-go/v21"
+	dctapi "github.com/delphix/dct-sdk-go/v22"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
