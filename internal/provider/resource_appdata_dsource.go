c814691f524ceb771f469af37f62eea7c7a9c95c
Uddipaan Hazarika <uddipaan.hazarika@perforce.com>
2025-03-07 23:04:12 +0530

    DLPXECO-11595 | upgraded to sdk2025.1.2

--- internal/provider/resource_appdata_dsource.go
+++ internal/provider/resource_appdata_dsource.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"net/http"
 
-	dctapi "github.com/delphix/dct-sdk-go/v22"
+	dctapi "github.com/delphix/dct-sdk-go/v25"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
